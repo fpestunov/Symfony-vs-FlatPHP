4ba2b16653a164629b6630cee54ff01d801d4e53
Fedor Pestunov <fpestunov@gmail.com>
2020-11-02 19:06:59 +0400

    add front-controller

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 require_once 'model.php';
+require_once 'controller.php';
 
-$posts = get_all_posts();
-
-require 'templates/list.php';
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if ('/index.php' === $uri || '/' === $uri) {
+  list_action();
+} elseif ('/show.php' === $uri && isset($_GET['id'])) {
+  show_action($_GET['id']);
+} else {
+  header('HTTP/1.1 404 Not Found');
+  echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
